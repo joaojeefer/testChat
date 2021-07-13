f71482a54416fd215209200442a50b0c1e81e448
João Jeefer <joao.felix@dbserver.com.br>
2021-07-13 19:06:20 -0300

    add cores aos estilos globais

---
+++ src/styles/colors.ts
@@ -0,0 +1,27 @@
+const colors = {
+  primary: {
+    dark: '#1e90ff',
+    default: '#00bfff',
+  },
+  secondary: {
+    darkest: '#000000',
+    darker: '#222222',
+    dark: '#5a645a',
+    default: '#808080',
+    light: '#dcdcdc',
+    lighter: '#f8f9f7',
+    lightest: '#ffffff',
+  },
+  warning: {
+    dark: '#765f00',
+    default: '#ffcd00',
+    light: '#ffeb98',
+  },
+  danger: {
+    dark: '#ab4745',
+    default: '#dc143c',
+    light: '#ffb4b4',
+  },
+};
+
+export default colors;
